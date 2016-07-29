cbba0aceed16a7d1ccc6d44515a494d009394a22
Vadim Kaushan <admin@disasm.info>
2016-07-29 13:23:20 +0300

    New ProcessDataModel

--- app/src/ProcessDataModel.cpp
+++ app/src/ProcessDataModel.cpp
@@ -7,16 +7,66 @@ ProcessDataModel::ProcessDataModel()
     m_items.append(qMakePair(QString("dont_kill_me_please"), 1));
 }
 
-int ProcessDataModel::size() const
+QHash<int, QByteArray> ProcessDataModel::roleNames() const
 {
+    QHash<int, QByteArray> roles;
+    roles[Qt::DisplayRole] = "name";
+    roles[Qt::UserRole] = "load";
+    return roles;
+
+}
+
+int ProcessDataModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
     return m_items.size();
 }
 
-QVariant ProcessDataModel::get(int index) const
+QVariant ProcessDataModel::data(const QModelIndex &index, int role) const
 {
-    QVariantMap m;
-    m["name"] = m_items[index].first;
-    m["load"] = m_items[index].second;
-    return m;
+    int row = index.row();
+    if (row < 0 || row >= m_items.size()) return QVariant();
+
+    if (role == Qt::DisplayRole)
+    {
+        return m_items[row].first;
+    }
+    if (role == Qt::UserRole)
+    {
+        return m_items[row].second;
+    }
+    return QVariant();
+}
+
+void ProcessDataModel::setLogFile(LogFile *logFile)
+{
+    qWarning("setLogFile");
+    m_logFile = logFile;
+    connect(m_logFile, SIGNAL(updated()), this, SLOT(logFileChanged()));
+}
+
+void ProcessDataModel::logFileChanged()
+{
+    layoutAboutToBeChanged();
+
+    qWarning("LogFile changed");
+    if (m_logFile == 0) return;
+
+    m_items.clear();
+
+    /*QMap<QString, int> oldTime;
+
+    QList<LogFile::LogRecord> records = m_logFile->records();
+    foreach(const LogFile::LogRecord &r, records)
+    {
+        foreach (const LogFile::ProcessRecord &rp, r.proc)
+        {
+            //
+        }
+
+        //m_items.append(item);
+    }*/
+
+    layoutChanged();
 }
 
